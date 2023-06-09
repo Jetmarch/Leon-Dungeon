158726ac65a16c88c397e5fcca98815a1d6790d2
Andrew Kritz <j3tmarch@gmail.com>
2023-06-09 22:26:10 +0300

    UIItemHolder fix

--- Assets/Scripts/Core/Game/Inventory/UI/UIItemHolder.cs
+++ Assets/Scripts/Core/Game/Inventory/UI/UIItemHolder.cs
@@ -7,26 +7,21 @@ public class UIItemHolder : MonoBehaviour
 {
     public Item item;
 
-    [Header("Events")]
-    [SerializeField] private SOEvent itemUse;
-    [SerializeField] private SOEvent itemEquip;
-    [SerializeField] private SOEvent itemUnequip;
-
     public void OnUseItem()
     {
         Debug.Log($"Item {item.name} was used");
-        itemUse.Raise(new SOEventArgOne<Item>(item));
+        SOEventKeeper.Instance.GetEvent("onItemUse").Raise(new SOEventArgOne<Item>(item));
     }
 
     public void OnEquipItem()
     {
         Debug.Log($"Item {item.name} was equiped");
-        itemEquip.Raise(new SOEventArgOne<Item>(item));
+        SOEventKeeper.Instance.GetEvent("onItemEquip").Raise(new SOEventArgOne<Item>(item));
     }
 
     public void OnUnequipItem()
     {
         Debug.Log($"Item {item.name} was unequiped");
-        itemUnequip.Raise(new SOEventArgOne<Item>(item));
+        SOEventKeeper.Instance.GetEvent("onItemUnequip").Raise(new SOEventArgOne<Item>(item));
     }
 }
