d173b7c9593154e03b8b042ac0d6ee20c2d9aaac
Andrew Kritz <j3tmarch@gmail.com>
2023-07-25 17:34:28 +0300

    fix: added SerializeField tag to ActorStats fields

--- Assets/Scripts/Core/Game/Actor/ActorStats.cs
+++ Assets/Scripts/Core/Game/Actor/ActorStats.cs
@@ -1,24 +1,32 @@
 using System;
-
+using UnityEngine;
 
 [Serializable]
 public class ActorStats
 {
-    private float strength;
-    private float agility;
-    private float speed;
-    private float sturdiness;
-    private float mind;
-    private float intelligence;
-    private float perception;
-    private float luck;
+
+    [SerializeField] private float strength;
+    [SerializeField] private float agility;
+    [SerializeField] private float speed;
+    [SerializeField] private float sturdiness;
+    [SerializeField] private float mind;
+    [SerializeField] private float intelligence;
+    [SerializeField] private float perception;
+    [SerializeField] private float luck;
 
     public float Strength { get { return strength; } private set { strength = value; } }
+    
     public float Agility { get { return agility; } private set { agility = value; } }
+    
     public float Speed { get { return speed; } private set { speed = value; } }
+    
     public float Sturdiness { get { return sturdiness; } private set { sturdiness = value; } }
+    
     public float Mind { get { return mind; } private set { mind = value; } }
+    
     public float Intelligence { get { return intelligence; } private set { intelligence = value; } }
+    
     public float Perception { get { return perception; } private set { perception = value; } }
+    
     public float Luck { get { return luck; } private set { luck = value; } }
 }
