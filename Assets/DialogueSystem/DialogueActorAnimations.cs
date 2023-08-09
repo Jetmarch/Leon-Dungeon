bed31015a8002a535b9e9e87a71170c8af28b209
Andrew Kritz <j3tmarch@gmail.com>
2023-08-09 22:22:49 +0300

    feat: added class DialogueActorAnimations

---
+++ Assets/DialogueSystem/DialogueActorAnimations.cs
@@ -0,0 +1,18 @@
+using MoreMountains.Feedbacks;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+/// <summary>
+/// 
+/// </summary>
+public class DialogueActorAnimations : MonoBehaviour
+{
+    [SerializeField] private MMF_Player bumpAnimation;
+}
+
+public enum DialogueActorAnimationType
+{
+    Bump
+}
